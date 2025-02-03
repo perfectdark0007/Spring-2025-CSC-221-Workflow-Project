f5cb410630bf87e4707e56175358c9621655ba22
perfectdark0007 <woodardwjun@gmail.com>
2025-02-03 12:22:44 -0500

    Branch 2

--- Spring 2025 CSC 221 Workflow Project/Spring 2025 CSC 221 Workflow Project.cpp
+++ Spring 2025 CSC 221 Workflow Project/Spring 2025 CSC 221 Workflow Project.cpp
@@ -1,11 +1,18 @@
 // Spring 2025 CSC 221 Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
+#include <iostream> 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name, address, phone;
+	name = "WILLIAM WOODARD";
+	address = "6 THOMAS, POQUOSON, VA"; 
+		phone = "757-868-6740";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phone << endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
