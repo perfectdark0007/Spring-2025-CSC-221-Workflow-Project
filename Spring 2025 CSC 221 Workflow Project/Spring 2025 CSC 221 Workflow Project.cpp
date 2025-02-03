51714dc4dfdeb1ec03ad1f48a47dfdccb36f1144
perfectdark0007 <woodardwjun@gmail.com>
2025-02-03 12:16:54 -0500

    Branch 1

--- Spring 2025 CSC 221 Workflow Project/Spring 2025 CSC 221 Workflow Project.cpp
+++ Spring 2025 CSC 221 Workflow Project/Spring 2025 CSC 221 Workflow Project.cpp
@@ -1,11 +1,16 @@
 // Spring 2025 CSC 221 Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
+#include <iostream> 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	char first, middle, last;
+	first = 'W';
+	middle = 'J';
+	last = 'W';
+	cout << first << middle << last;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
